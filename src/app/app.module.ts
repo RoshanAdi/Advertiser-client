25dd5c64e03cac6ee1299e6d6516f26d7d5ab8b8
adikari <rohshanmahesh1@gmail.com>
2023-03-31 21:52:05 +0530

    login page & Registration page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import {FormsModule} from "@angular/forms";
+import {AlertModule} from "ngx-bootstrap/alert";
+import {HttpClientModule} from "@angular/common/http";
+import {RouterModule, Routes} from "@angular/router";
 
+const routes: Routes=[
+  {path:'login',component:LoginComponent},
+  {path:'register',component:SignUpComponent},
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignUpComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [AlertModule.forRoot(),RouterModule.forRoot(routes),
+        BrowserModule,
+        FormsModule,HttpClientModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
