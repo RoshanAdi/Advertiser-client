7e6f005a6e5ff4a7eeae03b6d498eacad93fbcbd
adikari <rohshanmahesh1@gmail.com>
2023-04-13 19:01:30 +0530

    post payment details

--- src/app/Checkout/checkout/checkout.component.ts
+++ src/app/Checkout/checkout/checkout.component.ts
@@ -1,7 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {NgForm} from "@angular/forms";
 import {CheckoutService} from "../../Services/checkout.service";
-
+import {Customer, CurrencyType, PayhereCheckout, CheckoutParams, Payhere, AccountCategory} from 'payhere-js-sdk'
+import {retry} from "rxjs";
+import {CheckoutParamsType} from "payhere-js-sdk/lib/interfaces";
+Payhere.init("1222880",AccountCategory.SANDBOX)
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
@@ -56,8 +59,52 @@ export class CheckoutComponent implements OnInit{
       this.amount=this.amount+this.cart.get(price)
     }
   }
+  @ViewChild('sendForm') sendForm1 : any;
+
   sendCheckOutForm(checkOutDetails:NgForm){
     console.log(checkOutDetails)
-    this.checkoutService.checkout(checkOutDetails)
+    this.checkoutService.checkout(checkOutDetails).subscribe(res=> {
+        console.warn("feedback = "+JSON.stringify(res))
+      this.checkout(JSON.parse(JSON.stringify(res)))
+
+      },
+
+    );
+
+  }
+  onPayhereCheckoutError(errorMsg:any) {
+    alert(errorMsg)
+  window.location.reload()
+  }
+
+  checkout(data:any) {
+    console.warn(JSON.stringify(data))
+    const customer = new Customer({
+      first_name: data.first_name.toString(),
+      last_name: data.last_name.toString(),
+      phone: data.phone.toString(),
+      email: data.email.toString(),
+      address: data.address.toString(),
+      city: data.city.toString(),
+      country: data.country.toString(),
+
+    })
+
+    const checkoutData = new CheckoutParams(<CheckoutParamsType>{
+
+
+      returnUrl: data.return_url.toString(),
+      cancelUrl: data.cancel_url.toString(),
+      notifyUrl: data.notify_url.toString(),
+      order_id: data.order_id.toString(),
+      itemTitle: data.items.toString(),
+      currency: CurrencyType.LKR,
+      amount: data.amount.toString(),
+      hash:data.hash.toString(),
+
+    })
+    console.error(checkoutData)
+    const checkout = new PayhereCheckout(customer,checkoutData,this.onPayhereCheckoutError)
+    checkout.start()
   }
 }
--- src/app/Services/checkout.service.ts
+++ src/app/Services/checkout.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {NgForm} from "@angular/forms";
+import {Observable} from "rxjs";
 const AUTH_API = 'http://localhost:8082/';
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -11,10 +13,9 @@ export class CheckoutService {
 
   constructor(private http: HttpClient) { }
 
-  checkout(checkoutData: any){
-    this.http.post(AUTH_API + 'checkout', checkoutData, httpOptions).subscribe((result) => {
-      console.log(result)
-    });
+  checkout(checkoutData: string |NgForm){
+    console.warn("trigerring")
+    return this.http.post(AUTH_API + 'checkout', checkoutData,httpOptions);
   }
 
 }
